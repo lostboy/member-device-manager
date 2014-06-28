9d566de75ec5d078724282d0c794dcbb676d82d2
Espen Høgbakk <espen@hogbakk.no>
2014-06-28 17:53:15 +0200

    Remove permit params
    We don’t need permit params, as ActiveAdmin is configured to allow all
    params.

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
-
   index do
     selectable_column
     id_column
@@ -24,5 +22,4 @@ ActiveAdmin.register AdminUser do
     end
     f.actions
   end
-
 end
