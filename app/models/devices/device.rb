70d83ac5aed05c3fb3ea1933d261afe0d4cf642d
Espen Høgbakk <espen@hogbakk.no>
2014-06-29 18:02:33 +0200

    Validate presence of users.

--- app/models/devices/device.rb
+++ app/models/devices/device.rb
@@ -2,6 +2,7 @@ class Devices::Device < ActiveRecord::Base
   belongs_to :user
   belongs_to :type, class_name: 'Devices::Type'
 
+  validates :user, presence: true
   validates :type, presence: true
   validates :mac_address, presence: true
 end
