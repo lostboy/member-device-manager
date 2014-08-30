9466e728c1b1a9f4868902b48dcef4ddb9634a8c
Espen Høgbakk <espen@hogbakk.no>
2014-08-30 06:51:30 +0200

    Add full name method to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true, email: true
   validates :first_name, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def to_s
+    self.full_name
+  end
 end
