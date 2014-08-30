d3048f8deb8bdbb38bed94ac4c59a15ade754c3e
Espen Høgbakk <espen@hogbakk.no>
2014-08-30 10:48:00 +0200

    Add todo on user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# TODO: Watch for updates on the following fields:
+# * first_name
+# * last_name
+# * email
+# * membership_status
+# * membership_level
+# * devices -> device_type
+# * devices -> mac_address
+# * devices -> ip_address
+#
+# When they update, trigger router update (delete/add)
 
 class User < ActiveRecord::Base
   has_many :devices, class_name: 'Devices::Device'
