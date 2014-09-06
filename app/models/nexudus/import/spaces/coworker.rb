1bd16498cc4892a17e17dc809cde701d1088c9a3
Espen Høgbakk <espen@hogbakk.no>
2014-09-06 10:45:37 +0200

    Refactor coworker update script.

--- app/models/nexudus/import/spaces/coworker.rb
+++ app/models/nexudus/import/spaces/coworker.rb
@@ -8,19 +8,10 @@ class Nexudus::Import::Spaces::Coworker
   end
 
   # Updates members with data from the Nexudus API
-  #
-  # If we have some members in the database, only ask for coworkers updated since
-  # the last updated members in the database.
   def update
-    if Member.count > 0
-      timestamp = Member
-        .select(:nexudus_updated_at)
-        .order(nexudus_updated_at: :desc)
-        .first
-        .nexudus_updated_at
-
+    if last_updated_at
       params = {
-        'from_Coworker_UpdatedOn' => timestamp.iso8601
+        'from_Coworker_UpdatedOn' => last_updated_at.iso8601
       }
     else
       params = nil
@@ -29,24 +20,13 @@ class Nexudus::Import::Spaces::Coworker
     # The api is paginated, so we need to loop through all pages
     complete = false
     until complete
+      # Fetch coworkers
       response = @client.coworkers(params)
-      response['Records'].each.map do |coworker|
-        member = Member.find_or_initialize_by nexudus_id: coworker['UniqueId']
 
-        member.first_name, member.last_name = coworker['FullName'].split(" ", 2)
-        member.email                      = coworker['Email']
-        member.nexudus_user_id            = coworker['UserID']
-        member.nexudus_updated_at         = coworker['UpdatedOn']
-        member.nexudus_created_at         = coworker['CreatedOn']
-        #member.membership_level           = coworker['']
-        #member.membership_renewal_date    = coworker['']
-        #member.membership_status          = coworker['']
-        member.active                     = coworker['Active']
+      # Import coworkers
+      import response['Records']
 
-        member.save!
-        member
-      end
-
+      # Handle pagination
       currentPage = response['CurrentPage']
       totalPages = response['TotalPages']
 
@@ -56,6 +36,40 @@ class Nexudus::Import::Spaces::Coworker
         nextPage = currentPage += 1
         { 'page' => nextPage }.merge! params || {}
       end
+    end
+  end
+
+  private
+
+  # Retrive timestamp for last updated member
+  def last_updated_at
+    if Member.count > 0
+      Member
+        .select(:nexudus_updated_at)
+        .order(nexudus_updated_at: :desc)
+        .first
+        .nexudus_updated_at
+    else
+      nil
+    end
+  end
+
+  # Save given coworkers to the database
+  def import coworkers
+    coworkers.each do |coworker|
+      member = Member.find_or_initialize_by nexudus_id: coworker['UniqueId']
+
+      member.first_name, member.last_name = coworker['FullName'].split(" ", 2)
+      member.email                        = coworker['Email']
+      member.nexudus_user_id              = coworker['UserID']
+      member.nexudus_updated_at           = coworker['UpdatedOn']
+      member.nexudus_created_at           = coworker['CreatedOn']
+      #member.membership_level             = coworker['']
+      #member.membership_renewal_date      = coworker['']
+      #member.membership_status            = coworker['']
+      member.active                       = coworker['Active']
+
+      member.save!
     end
   end
 end
