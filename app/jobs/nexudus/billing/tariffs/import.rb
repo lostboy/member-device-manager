70b17606299e3089499f480f63dfdba319e2d594
Espen Høgbakk <espen@hogbakk.no>
2014-09-07 11:08:36 +0200

    Add Sidekiq job for retrieving tariffs.

---
+++ app/jobs/nexudus/billing/tariffs/import.rb
@@ -0,0 +1,11 @@
+module Nexudus::Jobs::Billing
+  class Tariff
+    include Sidekiq::Worker
+
+    sidekiq_options queue: :nexudus, retry: true
+
+    def perform
+      Nexudus::Import::Billing::Tariff.instance.update
+    end
+  end
+end
