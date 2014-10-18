372443aa014718759c9db87963d9931907befc15
Espen Høgbakk <espen@hogbakk.no>
2014-10-18 16:35:21 +0200

    Add some helper methods to admin to renew/release ip addresses.

--- app/admin/devices/device.rb
+++ app/admin/devices/device.rb
@@ -1,2 +1,38 @@
 ActiveAdmin.register Devices::Device, as: 'Device' do
+
+  member_action :renew do
+    resource.renew!
+    redirect_to :back, notice: I18n.t("admin.renewed", resource: resource.class.model_name.human)
+  end
+
+  member_action :release do
+    resource.release!
+    redirect_to :back, notice: I18n.t("admin.renewed", resource: resource.class.model_name.human)
+  end
+
+  index do
+    selectable_column
+    id_column
+
+    column :mac_address
+    column :ip_address
+    column :updated_at
+    column :created_at
+    actions defaults: true do |device|
+      links = []
+
+      links << link_to(I18n.t("admin.renew", default: "Renew"), renew_admin_device_path(device.id))
+      links << link_to(I18n.t("admin.release", default: "Release"), release_admin_device_path(device.id))
+
+      links.join(" ").html_safe
+    end
+  end
+
+  action_item only: :show do
+    link_to(I18n.t("admin.renew", default: "Renew"), renew_admin_device_path(resource.id))
+  end
+
+  action_item only: :show do
+    link_to(I18n.t("admin.release", default: "Release"), release_admin_device_path(resource.id))
+  end
 end
