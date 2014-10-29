722168172c4366985271211284c5b874836af3b7
Espen Høgbakk <espen@hogbakk.no>
2014-10-29 10:02:40 +0100

    Make sure you need authentication to see anything
    Also display which user is currently logged inn, and allow logging out.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  authenticate :admin_user, ->(user) { Ability.new(user).can? :read, :sidekiq } do
+  ActiveAdmin.routes self
+
+  authenticate :admin_user do
     mount Sidekiq::Web => '/sidekiq'
   end
-
-  ActiveAdmin.routes self
 
   root 'home#dashboard'
   resources :members, only: %i(index show update)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019080045) do
+ActiveRecord::Schema.define(version: 20141029085440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20141019080045) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "roles_mask"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
@@ -88,5 +90,16 @@ ActiveRecord::Schema.define(version: 20141019080045) do
     t.datetime "updated_at"
     t.string   "network"
   end
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
