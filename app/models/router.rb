78b735123cca2dbff22d556aed951ec456941f5c
Espen Høgbakk <espen@hogbakk.no>
2014-10-29 05:24:43 +0100

    Update router to work with Hubud router spec

--- app/models/router.rb
+++ app/models/router.rb
@@ -2,40 +2,50 @@ require 'net/ssh'
 
 class Router
   SCRIPTS = {
-    add: 'rt.add.device',
-    remove: 'rt.remove.device',
-    disable: 'rt.disable.device',
-    enable: 'rt.enable.device',
-    dhcp: 'rt.mgr.dhcp'
+    add: 'manager.rt',
+    remove: 'manager.rt',
+    disable: 'devable.rt',
+    enable: 'devable.rt'
+  }
+
+  ARGUMENTS_MAPPING = {
+    enable: '-e',
+    disable: '-d',
+    show: '-s',
+    first_name: '-f',
+    last_name: '-l',
+    email: '-e',
+    device_type: '-t',
+    mac_address: '-m',
+    ip_address: '-i',
+    hotspot: '-h'
   }
 
   def initialize
+    @port     = Rails.application.secrets.router_port || 22
     @hostname = Rails.application.secrets.router_host
     @username = Rails.application.secrets.router_username
     @password = Rails.application.secrets.router_password
   end
 
   def register device
-    device_info = [
-      device.member.first_name,
-      device.member.last_name,
-      device.member.email,
-      device.type.kind,
-      device.mac_address,
-      device.ip_address,
-      device.type.hotspot
-    ]
-
+    device_info = {
+      first_name: device.member.first_name,
+      last_name: device.member.last_name,
+      email: device.member.email,
+      device_type: device.type.kind,
+      mac_address: device.mac_address,
+      ip_address: device.ip_address,
+      hotspot: device.type.hotspot
+    }
     # TODO: Make sure everything is not nil
 
-    argument = device_info.join(",")
-
-    ssh(SCRIPTS[:add], argument)
+    ssh(SCRIPTS[:add], hash_to_arguments(device_info))
   end
 
   def unregister device
-    argument = device.mac_address
-    ssh(SCRIPTS[:remove], argument)
+    device_info = { mac_address: device.mac_address }
+    ssh(SCRIPTS[:remove], hash_to_arguments(device_info))
   end
 
   def update device
@@ -44,36 +54,53 @@ class Router
   end
 
   def enable device
-    argument = device.mac_address
-    ssh(SCRIPTS[:enable], argument)
+    device_info = { mac_address: device.mac_address, enable: true }
+    ssh(SCRIPTS[:enable], hash_to_arguments(device_info))
   end
 
   def disable device
-    argument = device.mac_address
-    ssh(SCRIPTS[:disable], argument)
+    device_info = { mac_address: device.mac_address, disable: true }
+    ssh(SCRIPTS[:disable], hash_to_arguments(device_info))
   end
 
   def update_ip_address device
     device_info = [
-      device.member.first_name,
-      device.member.last_name,
-      device.member.email,
-      device.type.kind,
-      device.mac_address,
-      device.ip_address,
-      device.type.hotspot
+      first_name: device.member.first_name,
+      last_name: device.member.last_name,
+      email: device.member.email,
+      device_type: device.type.kind,
+      mac_address: device.mac_address,
+      ip_address: device.ip_address,
+      hotspot: device.type.hotspot
     ]
 
-    argument = device_info.join(",")
-
-    ssh(SCRIPTS[:dhcp], argument)
+    ssh(SCRIPTS[:dhcp], hash_to_arguments(device_info))
   end
 
-  def ssh(script, argument)
+  def ssh(script, arguments)
     unless Rails.env.to_sym == :test
-      Net::SSH.start(@host, @username, password: @password) do |ssh|
-        output = ssh.exec!("#{script} #{argument}")
+      Net::SSH.start(@host, port: @port, username: @username, password: @password) do |ssh|
+        output = ssh.exec!("#{script} #{arguments}")
       end
     end
+  end
+
+  # Convert a hash to the proper ssh script argument
+  def hash_to_arguments hash
+    # Remap the hash, with the correct ssh arguments
+    arguments = Hash[hash.map { |k, v| [ARGUMENTS_MAPPING[k], v] }]
+
+    # Remove blank values from hash
+    arguments.delete_if { |key, value| value.blank? }
+
+    # Downcase every value in the hash, except mac_address
+    arguments = Hash[arguments.map { |k, v| [k, v.to_s.downcase] }]
+    mac_address_key = ARGUMENTS_MAPPING[:mac_address]
+    if arguments[mac_address_key]
+      arguments[mac_address_key] = arguments[mac_address_key].upcase
+    end
+
+    # Convert the hash into a string by joining key and value
+    arguments.map { |e| e.join(" ") }.join(" ")
   end
 end
--- spec/models/router_spec.rb
+++ spec/models/router_spec.rb
@@ -6,25 +6,28 @@ describe Router do
 
   describe "#register" do
     it 'registers the device with the router' do
-      device_info = [
-        device.member.first_name,
-        device.member.last_name,
-        device.member.email,
-        device.type.kind,
-        device.mac_address,
-        device.ip_address,
-        device.type.hotspot
-      ]
-      argument = device_info.join(",")
+      device_info = {
+        first_name: device.member.first_name,
+        last_name: device.member.last_name,
+        email: device.member.email,
+        device_type: device.type.kind,
+        mac_address: device.mac_address,
+        ip_address: device.ip_address,
+        hotspot: device.type.hotspot
+      }
+      arguments = router.hash_to_arguments device_info
 
-      expect(router).to receive(:ssh).with("rt.add.device", argument)
+      expect(router).to receive(:ssh).with("manager.rt", arguments)
       router.register device
     end
   end
 
   describe "#unregister" do
     it 'unregisters the device with the router' do
-      expect(router).to receive(:ssh).with("rt.remove.device", device.mac_address)
+      device_info = { mac_address: device.mac_address }
+      arguments = router.hash_to_arguments device_info
+
+      expect(router).to receive(:ssh).with("manager.rt", arguments)
       router.unregister device
     end
   end
@@ -39,37 +42,64 @@ describe Router do
 
   describe "#enable" do
     it 'enables the device in the router' do
-      expect(router).to receive(:ssh).with("rt.enable.device", device.mac_address)
+      device_info = { mac_address: device.mac_address, enable: true }
+      arguments = router.hash_to_arguments device_info
+
+      expect(router).to receive(:ssh).with("devable.rt", arguments)
       router.enable device
     end
   end
 
   describe "#disable" do
     it 'disables the device in the router' do
-      expect(router).to receive(:ssh).with("rt.disable.device", device.mac_address)
+      device_info = { mac_address: device.mac_address, disable: true }
+      arguments = router.hash_to_arguments device_info
+
+      expect(router).to receive(:ssh).with("devable.rt", arguments)
       router.disable device
-    end
-  end
-
-  describe "#update_ip_address" do
-    it 'updates a devices ip address in the router' do
-      device_info = [
-        device.member.first_name,
-        device.member.last_name,
-        device.member.email,
-        device.type.kind,
-        device.mac_address,
-        device.ip_address,
-        device.type.hotspot
-      ]
-      argument = device_info.join(",")
-
-      expect(router).to receive(:ssh).with("rt.mgr.dhcp", argument)
-      router.update_ip_address device
     end
   end
 
   describe "#ssh" do
     it 'executes given ssh command'
+  end
+
+  describe "#hash_to_arguments" do
+    context 'all keys present' do
+      it 'converts a hash to the proper ssh arguments' do
+        hash = {
+          first_name: 'Mark',
+          last_name: 'Testlast',
+          email: 'user@test.com',
+          device_type: 'PC',
+          mac_address: 'B8:E8:56:0A:E8:AC',
+          ip_address: '10.10.10.110',
+          hotspot: 'hsv1-mem'
+        }
+        arguments = router.hash_to_arguments hash
+        expect(arguments).to eq("-f mark -l testlast -e user@test.com -t pc -m B8:E8:56:0A:E8:AC -i 10.10.10.110 -h hsv1-mem")
+      end
+    end
+
+    context 'some keys present' do
+      it 'converts a hash to the proper ssh arguments' do
+        hash = {
+          mac_address: 'B8:E8:56:0A:E8:AC'
+        }
+        arguments = router.hash_to_arguments hash
+        expect(arguments).to eq("-m B8:E8:56:0A:E8:AC")
+      end
+    end
+
+    context 'some keys blank present' do
+      it 'converts a hash to the proper ssh arguments' do
+        hash = {
+          mac_address: 'B8:E8:56:0A:E8:AC',
+          hotspot: ''
+        }
+        arguments = router.hash_to_arguments hash
+        expect(arguments).to eq("-m B8:E8:56:0A:E8:AC")
+      end
+    end
   end
 end
