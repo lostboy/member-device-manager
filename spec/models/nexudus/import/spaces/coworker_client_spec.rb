3225f0648b1666655be895f0bd20e31bbb4f6462
Espen Høgbakk <espen@hogbakk.no>
2014-11-04 10:05:50 +0100

    Add authentication to Nexudus API calls

--- spec/models/nexudus/import/spaces/coworker_client_spec.rb
+++ spec/models/nexudus/import/spaces/coworker_client_spec.rb
@@ -4,7 +4,15 @@ describe Nexudus::Import::Spaces::CoworkerClient do
   describe "#resources" do
     it "gets the coworkers" do
       expect(described_class).to receive(:get)
-        .with("/spaces/coworkers", {})
+        .with(
+          "/spaces/coworkers",
+          {
+            basic_auth: {
+              username: Rails.application.secrets.nexudus_username,
+              password: Rails.application.secrets.nexudus_password
+            }
+          }
+        )
 
       subject.resources
     end
